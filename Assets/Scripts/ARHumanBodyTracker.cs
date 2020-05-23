d03a2b292d25cc5606703ad2ffa0451285d5d677
paulboranian <pboranian@me.com>
2020-05-22 23:27:26 -0700

    Initial commit

---
+++ Assets/Scripts/ARHumanBodyTracker.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARHumanBodyTracker : MonoBehaviour
+{
+
+    public GameObject skeletonPrefab;
+    public ARHumanBodyManager humanBodyManager;
+
+    private Dictionary<TrackableId, HumanBoneController> skeletonTracker = new Dictionary<TrackableId, HumanBoneController>();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnHumanBodiesChanged(ARHumanBodiesChangedEventArgs eventArgs)
+    {
+        HumanBoneController humanBoneController;
+
+        foreach(var bodyPart in eventArgs.added)
+        {
+            if (!skeletonTracker.TryGetValue(bodyPart.trackableId, out humanBoneController))
+            {
+                var newSkeletonGameObject = Instantiate(skeletonPrefab, bodyPart.transform);
+                humanBoneController = newSkeletonGameObject.GetComponent<HumanBoneController>();
+                skeletonTracker.Add(bodyPart.trackableId, humanBoneController);
+            }
+
+            humanBoneController.InitializeSkeletonJoints();
+            humanBoneController.ApplyBodyPose(bodyPart, Vector3.zero);
+        }
+
+        foreach(var bodyPart in eventArgs.updated)
+        {
+            if (skeletonTracker.TryGetValue(bodyPart.trackableId, out humanBoneController))
+            {
+                humanBoneController.ApplyBodyPose(bodyPart, Vector3.zero);
+            }
+
+            ApplyClothing();
+        }
+
+
+    }
+
+    private void ApplyClothing()
+    {
+
+
+    }
+
+    private void OnEnable()
+    {
+        humanBodyManager.humanBodiesChanged += OnHumanBodiesChanged;
+    }
+
+    private void OnDisable()
+    {
+        humanBodyManager.humanBodiesChanged -= OnHumanBodiesChanged;
+    }
+}
